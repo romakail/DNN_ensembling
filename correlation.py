e53a878981bce30f51e01a1ff057b0670b5a4d2f
romakail <roman.kail@mail.ru>
2020-03-19 20:27:06 +0000

    Implemented correlation functions
    - Implemented functions which calculate correlation for every feature and correlation matrix for different models
    - Implemented some visualisation

---
+++ correlation.py
@@ -0,0 +1,56 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+from tqdm import tqdm_notebook as tqdm
+
+def predictions_correlation(prediction1, prediction2):
+    assert len(prediction1.shape) == 2
+    assert prediction1.shape == prediction2.shape
+
+    correlation = (
+        ( (prediction1 - prediction1.mean(dim=0, keepdim=True))
+        * (prediction2 - prediction2.mean(dim=0, keepdim=True))).mean(dim=0)
+        / prediction1.var(dim=0).sqrt()
+        / prediction2.var(dim=0).sqrt())
+    return correlation.cpu().numpy()
+
+def correlation_over_classes (model1, model2, dataloader, device=torch.device('cpu')):
+    model1.eval().to(device)
+    model2.eval().to(device)
+
+    prediction1 = []
+    prediction2 = []
+
+    for input, _ in dataloader:
+        prediction1.append(model1(input.detach().to(device)).detach().cpu())
+        prediction2.append(model2(input.detach().to(device)).detach().cpu())
+
+    prediction1 = torch.cat(prediction1, dim=0)
+    prediction2 = torch.cat(prediction2, dim=0)
+
+    return predictions_correlation(prediction1, prediction2)
+
+def correlation_among_models (models, dataloader, device, one_hot=False):
+
+    predictions = []
+    for iter, model in tqdm(enumerate(models)):
+        model.eval().to(device)
+        predictions.append([])
+        for input, _ in dataloader:
+            predictions[iter].append(model(input.detach().to(device)).detach().cpu())
+        predictions[iter] = torch.cat(predictions[iter], dim=0)
+
+    if one_hot:
+        print ('hui')
+        for idx, prediction in enumerate(predictions):
+            predictions[idx] = F.one_hot(prediction.argmax(dim=1)).float()
+
+    print(predictions[0][0, :])
+
+    n_models = len(models)
+    cor_matrix = np.zeros([n_models, n_models], dtype=float)
+    for i in range(n_models):
+        for j in range(i, n_models):
+            cor_matrix[i, j] = cor_matrix[j, i] = (
+                predictions_correlation(predictions[i], predictions[j]).mean())
+
