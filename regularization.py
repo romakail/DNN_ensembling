7fa5e0179d0e2bb44fa592a56e85b2474f5e201a
romakail <roman.kail@mail.ru>
2020-05-06 21:46:37 +0300

    Fixed issue with adaBoost and ability to save changes for weights
    - Still have to try it on the server

--- regularization.py
+++ regularization.py
@@ -3,6 +3,7 @@ import torch
 from copy import deepcopy
 from tqdm import tqdm
 
+
 from torch.nn.functional import softmax
 
 class TwoModelsMSE ():
@@ -12,7 +13,7 @@ class TwoModelsMSE ():
             p = p.detach()
         self.wd = weight_decay
         print (self.wd)
-        
+
     def reg (self, model):
         l2 = 0.0
         x_sum = 0.
@@ -22,7 +23,43 @@ class TwoModelsMSE ():
             y_sum += y.sum().item()
             l2 += torch.sum((x - y) ** 2)
         return - self.wd * l2
-    
+
+
+
+def linear_weigh_func(logits, labels, coef=1):
+    assert logits.shape[0] == labels.shape[0]
+    prediction = softmax(logits, dim=1)
+    probas = prediction.take(
+        (labels + torch.arange(0, prediction.numel(), prediction.shape[1])).cuda())
+    w = coef * (1 - probas)
+    if normalize:
+        w /= torch.sum(w)
+        w *= w.shape[0]
+    return w
+
+def exponential_weight_func(logits, labels, coef=1):
+    prediction = softmax(logits, dim=1)
+    probas = prediction.take(
+        (labels + torch.arange(0, prediction.numel(), prediction.shape[1])).cuda())
+    w = torch.exp(coef * (1 - probas))
+    if normalize:
+        w /= torch.sum(w)
+        w *= w.shape[0]
+    return w
+
+def adalast_weight_func(logits, labels, coef=1):
+    true_logits = logits.take(
+        (labels + torch.arange(0, logits.numel(), logits.shape[1])).cuda())
+    true_logits = true_logits * (logits.shape[1] - 1) / logits.shape[1] - logits.mean(dim=1)
+#             print (labels.device)
+#             print (logits.device)
+    correct = 2 * torch.eq(labels.cuda(), logits.argmax(dim=1)) - 1
+    w = torch.exp(- coef * true_logits * correct)
+    if normalize:
+        w /= torch.sum(w)
+        w *= w.shape[0]
+    return w
+
 class AdaBoost ():
     def __init__(self, normalize=False):
         self.normalize = normalize
@@ -43,97 +80,62 @@ class AdaBoost ():
             w /= torch.sum(w)
             w *= w.shape[0]
         return w
-            
+
+
+
 def dataset_weights_generator(model, coef, func_type=None, normalize=False, batch_size=64):
     print ('Func_type : ', '[', func_type, ']', sep='')
     if func_type == 'Lin':
-        def weight_func(coef, logits, label):
-            prediction = softmax(logits, dim=1)
-            probas = prediction.take(
-                (label + torch.arange(0, prediction.numel(), prediction.shape[1])).cuda())
-            w = coef * (1 - probas)
-            if normalize:
-                w /= torch.sum(w)
-                w *= w.shape[0]
-            return w
+        weight_func = linear_weigh_func
     elif func_type == 'Exp':
-        def weight_func(coef, logits, label):
-            prediction = softmax(logits, dim=1)
-            probas = prediction.take(
-                (label + torch.arange(0, prediction.numel(), prediction.shape[1])).cuda())
-            w = torch.exp(coef * (1 - probas))
-            if normalize:
-                w /= torch.sum(w)
-                w *= w.shape[0]
-            return w
+        weight_func = exponential_weight_func
     elif func_type == 'AdaLast':
-        def weight_func(coef, logits, label):
-            true_logits = logits.take(
-                (label + torch.arange(0, logits.numel(), logits.shape[1])).cuda())
-            true_logits = true_logits * (logits.shape[1] - 1) / logits.shape[1] - logits.mean(dim=1)
-#             print (label.device)
-#             print (logits.device)
-            correct = 2 * torch.eq(label.cuda(), logits.argmax(dim=1)) - 1
-            w = torch.exp(- coef * true_logits * correct)
-            if normalize:
-                w /= torch.sum(w)
-                w *= w.shape[0]
-            return w
+        weight_func = adalast_weight_func
     elif func_type == 'AdaBoost':
-        adaboost_cont = AdaBoost(normalize=normalize)
-        weight_func = adaboost_cont.weight_func
-#     elif func_type == 'GradBoost':
-        
+        adaboost_container = AdaBoost(normalize=normalize)
+        weight_func = adaboost_container.weight_func
+    elif func_type == 'GradBoost':
+        return None
     else:
         print ("I don't know this function, choose on of [Lin/Exp/AdaLast/AdaBoost]")
         return None
-    
+
     def weights_generator(dataset):
         with torch.no_grad():
-            weights = []
             input_batch = []
-            label_batch = []
-            weights     = []
+            # label_batch = []
 
-            true_labels = 0
+            logits = []
+            labels = []
+
+            # true_labels = 0
             for idx, (input, label) in enumerate(dataset):
                 input_batch.append(input.unsqueeze(0))
-                label_batch.append(label)
+                # label_batch.append(label)
+                labels.append(label)
                 if (idx+1) % batch_size == 0:
                     input_batch = torch.cat(input_batch, dim=0)
-                    logits = model(input_batch.cuda())
-                    weights.append(weight_func(coef, logits, torch.tensor(label_batch)))
+                    logits_batch = model(input_batch.cuda())
+                    # weights.append(weight_func(coef, logits, torch.tensor(label_batch)))
 #                     true_labels += torch.eq(
 #                         prediction.argmax(dim=1).cuda(),
 #                         torch.tensor(label_batch).cuda()).sum().item()
-    
+                    logits.append(logits_batch)
                     input_batch = []
-                    label_batch = []
-            
+
             input_batch = torch.cat(input_batch, dim=0)
-            prediction = softmax(model(input_batch.cuda()), dim=1)
-            weights.append(weight_func(coef, prediction, torch.tensor(label_batch)))
-            
-            weights = torch.cat(weights, dim=0)
+            logits_batch = model(input_batch.cuda())
+            logits.append(logits_batch)
+            # weights.append(weight_func(coef, prediction, torch.tensor(label_batch)))
+            # weights = torch.cat(weights, dim=0)
+
+            logits = torch.cat(logits, dim=0)
+            labels = torch.tensor(labels, dtype=torch.long)
+
+            weights = weight_func(logits, labels, coef=coef)
+
             print ('Shape :', weights.shape, 'Weights_sum :', weights.sum().item())
             print ('Max :', weights.max().item(), 'Min :', weights.min().item())
         return weights
-    
-    return weights_generator
 
-# Some minor changes
-                                       
-                                       
-                                       
-                                       
-                                       
-                                       
-                                       
-                                       
-                                       
-                                       
-                                       
-                                       
-                                       
-                                       
-                                       
+    return weights_generator
